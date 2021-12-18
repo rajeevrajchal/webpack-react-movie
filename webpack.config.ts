7eb72794424e5c49bf287f6029b4a4c537f8d2e4
Rajeev Rajchal <rar@trainingtube.com>
2021-12-18 21:12:56 +0545

    minor changes

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,7 +4,6 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import TsconfigPathsPlugin from 'tsconfig-paths-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
-import Dotenv from 'dotenv-webpack';
 
 const webpackConfig = (): Configuration => ({
     entry: './src/index.tsx',
@@ -16,7 +15,8 @@ const webpackConfig = (): Configuration => ({
         plugins: [new TsconfigPathsPlugin({ configFile: './tsconfig.json' })],
     },
     output: {
-        path: path.join(__dirname, '/build'),
+        path: path.resolve(__dirname, 'build'),
+        publicPath: '/',
         filename: 'build.js',
     },
     module: {
@@ -76,7 +76,8 @@ const webpackConfig = (): Configuration => ({
     plugins: [
         new HtmlWebpackPlugin({
             // HtmlWebpackPlugin simplifies creation of HTML files to serve your webpack bundles
-            template: './public/index.html',
+            // template: './public/index.html',
+            template: path.resolve('./index.html'),
         }),
         // DefinePlugin allows you to create global constants which can be configured at compile time
         new DefinePlugin({
